fd3ca19e0fcbeb1688f586942c8156477eabfd91
Makaronodentro <Makaronodentro@Makaronodentros-MacBook-Pro.local>
2015-11-01 00:47:29 +0000

    added site

---
+++ LAproblems/templates/countyHelp_form.php
@@ -0,0 +1,26 @@
+<p>Your Local Authority's Health services rank is: <?= htmlspecialchars($vars["healthRank"]) ?></p>
+<p>Your Local Authority's Educational services rank is: <?= htmlspecialchars($vars["educationRank"]) ?></p>
+<p>Your Local Authority's Crime rate rank is: <?= htmlspecialchars($vars["crimeRank"]) ?></p>
+
+<!-- 
+<table>
+
+	<tr>
+		<td><?= htmlspecialchars($vars["health"]) ?></td>
+		<td>More</td>
+		<td>Start a charity!</td>
+	</tr>
+
+	<tr>
+		<td><?= htmlspecialchars($vars["socioeconomic"]) ?></td>
+		<td>More</td>
+		<td>Start a charity!</td>
+	</tr>
+
+	<tr>
+		<td><?= htmlspecialchars($vars["environmental"]) ?></td>
+		<td>More</td>
+		<td>Start a charity!</td>
+	</tr>
+</table>
+-->
---
+++ LAproblems/templates/home.php
@@ -0,0 +1,5 @@
+  <p>Care about your county? Find out how you can best help it!</p>
+  <form action="index.php" method="post">
+	  <input name="name" type="text" placeholder="Your county" required autofocus>
+	  <input type="submit" class="btn-login">
+  </form>
