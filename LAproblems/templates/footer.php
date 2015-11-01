ea1c5080e40359da7c0fd439ece917ca0c8fdee3
Makaronodentro <Makaronodentro@Makaronodentros-MacBook-Pro.local>
2015-11-01 07:54:35 +0000

    sasd"wq :wqwq

--- LAproblems/templates/footer.php
+++
@@ -1,8 +0,0 @@
-			<div class="push"></div>
-
-            <div id="bottom" class="center">
-                <p>Copyright &#169; 2015 County Help | All rights reserved. | T&Cs apply.</p>
-            </div>
-    </body>
-
-</html>
